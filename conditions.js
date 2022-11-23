d18be7f3595514e42ae30eb9d4f3065faf727efa
therenashah <therenashah@gmail.com>
2022-11-23 06:58:36 +0000

    Create conditions.js and cowsay.js

---
+++ conditions.js
@@ -0,0 +1,4 @@
+const a = true
+const names = ["rena1", "rena2", "rena3"]
+for(index in names){
+console.log(names.filter(name => name.length==5))}
---
+++ cowsay.js
@@ -0,0 +1,10 @@
+const cowsay = require("cowsay");
+const quote = require("inspirational-quotes");
+console.log(quote.getQuote().text);
+console.log(
+    cowsay.say({
+        text : text,
+        e : "xx",
+        T : "U"
+    })
+)
